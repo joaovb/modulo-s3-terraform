f5d16fbf5f0811939b15f0d6a2d0f6a938cd502b
João Victor Barreto <joaovictor.hts@gmail.com>
2024-04-06 22:01:54 -0300

    Updates for tests

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,50 +2,45 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = ">=5.44"
+      version = ">=3.0"
     }
   }
-
   required_version = ">=0.14.9"
+}
 
 provider "aws" {
   version = "~>3.0"
-  region  = "east-us-1"
+  region  = "us-east-1"
 }
 
 backend "s3" {
        bucket = "labs3site"
        key    = "index.html"
        region = "east-us-1"
-   }
 }
-
-}
-
-
 
 resource "aws_s3_bucket" "s3Bucket" {
-     bucket = "labs3site"
-     acl       = "public-read"
+  bucket = "labs3site"
+  acl    = "public-read"
 
-     policy  = <<EOF
-{
-     "id" : "MakePublic",
-   "version" : "2012-10-17",
-   "statement" : [
-      {
-         "action" : [
-             "s3:GetObject"
-          ],
-         "effect" : "Allow",
-         "resource" : "arn:aws:s3:::labs3site/*",
-         "principal" : "*"
-      }
-    ]
+  website {
+    index_document = "index.html"
   }
-EOF
 
-   website {
-       index_document = "index.html"
-   }
+  policy = <<EOF
+{
+  "Id" : "MakePublic",
+  "Version" : "2012-10-17",
+  "Statement" : [
+    {
+      "Action" : [
+        "s3:GetObject"
+      ],
+      "Effect" : "Allow",
+      "Resource" : "arn:aws:s3:::labs3site/*",
+      "Principal" : "*"
+    }
+  ]
+}
+EOF
 }
