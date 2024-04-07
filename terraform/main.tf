68f7b2ceae072bfe4f1bbd584351ec4036d75cf1
João Victor Barreto <joaovictor.hts@gmail.com>
2024-04-06 21:28:44 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,9 +8,42 @@ terraform {
 
   required_version = ">=0.14.9"
 
+backend "s3" {
+       bucket = "labs3site"
+       key    = "index.html"
+       region = "east-us-1"
+   }
+}
+
 }
 
 provider "aws" {
   version = "~>3.0"
   region  = "east-us-1"
+}
+
+resource "aws_s3_bucket" "s3Bucket" {
+     bucket = "labs3site"
+     acl       = "public-read"
+
+     policy  = <<EOF
+{
+     "id" : "MakePublic",
+   "version" : "2012-10-17",
+   "statement" : [
+      {
+         "action" : [
+             "s3:GetObject"
+          ],
+         "effect" : "Allow",
+         "resource" : "arn:aws:s3:::labs3site/*",
+         "principal" : "*"
+      }
+    ]
+  }
+EOF
+
+   website {
+       index_document = "index.html"
+   }
 }
